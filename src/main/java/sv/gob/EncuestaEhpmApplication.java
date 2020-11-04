fb36f91735201f3adb5741fee714278eb33168bf
MH17015 <MH17015@ues.edu.sv>
2020-11-04 16:11:56 -0600

    commit inicial

---
+++ src/main/java/sv/gob/EncuestaEhpmApplication.java
@@ -0,0 +1,13 @@
+package sv.gob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuestaEhpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuestaEhpmApplication.class, args);
+	}
+
+}

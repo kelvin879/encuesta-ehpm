f40e7c9000aa8d7aac4686df37ba4d8f49121565
Pablo Avelar <am16015@ues.edu.sv>
2020-11-24 17:00:46 -0600

    Modificando grupo

--- src/main/java/sv/gob/controller/administrador/GrupoController.java
+++ src/main/java/sv/gob/controller/administrador/GrupoController.java
@@ -6,34 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import sv.gob.models.Grupos;
 import sv.gob.service.administrador.IGrupoService;
 
 @Controller
+@RequestMapping("/grupo")
 public class GrupoController {
 	
 	@Autowired
 	private IGrupoService grupo;
 	
-	@GetMapping("/grupo/create")
+	@GetMapping("/crear")
 	private String CrearGrupo(Model model)
 	{
-		return "grupo01/Grupo";
+		return "grupo/Crear";
 	}
 	
-	@GetMapping("/grupo/update")
+	@GetMapping("/editar")
 	private String EditarGrupo(Model model)
 	{
-		return "grupo01/Grupo";
+		return "grupo/Editar";
 	}
 	
-	@GetMapping("/grupos")
+	@GetMapping("/administrar")
 	private String MostrarGrupo(Model model)
 	{
 		List<Grupos> listaMostrarGrupo = grupo.mostrar2();
+
 		model.addAttribute("mostrarGrupo",listaMostrarGrupo);
-		return "grupo01/AdministrarGrupo";
+
+		return "grupo/Administrar";
 	}
 
 }

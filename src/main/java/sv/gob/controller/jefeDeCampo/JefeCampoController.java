fb73b93a2d0b5ebd92c8942e8ada7302d592f30e
Pablo Avelar <am16015@ues.edu.sv>
2020-12-01 18:37:28 -0600

    Estilo agregado a encuestas pendientes de jefe de campo

--- src/main/java/sv/gob/controller/jefeDeCampo/JefeCampoController.java
+++ src/main/java/sv/gob/controller/jefeDeCampo/JefeCampoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import sv.gob.models.PilaDeEncuestas;
 import sv.gob.service.jefeDeCampo.IJefeCampoService;
@@ -13,6 +14,7 @@ import sv.gob.models.EncuestasPendientes;
 
 
 @Controller
+@RequestMapping("/jefeDeCampo")
 public class JefeCampoController {
 	
 	@Autowired
@@ -29,12 +31,13 @@ public class JefeCampoController {
 		return "desechadas/PilaEncuestas";
 	}
 	
-	@GetMapping("/pendiente")
+	@GetMapping("/home")
 	private String EncuestasPendientes(Model model)
 	{
 		List<EncuestasPendientes> lista2 = pendiente.mostrar2();
 		model.addAttribute("encuestas2",lista2);
-		return "grupo01/EncuestasPendientes";
+		
+		return "jefeDeCampo/EncuestasPendientes";
 	}
 
 }

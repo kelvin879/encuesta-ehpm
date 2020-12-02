bdf03f0bdd2913e435bcd171775e41bb38ea29f6
MH17015 <MH17015@ues.edu.sv>
2020-12-02 15:47:50 -0600

    Revisar y revisión de secciones y preguntas

--- src/main/java/sv/gob/controller/revision/ControlEncuestaController.java
+++ src/main/java/sv/gob/controller/revision/ControlEncuestaController.java
@@ -8,13 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import sv.gob.models.grupo02.Encuesta;
+import sv.gob.models.Asignacion02;
 import sv.gob.service.controlCalidad.IControlEncuestaService;
+import sv.gob.service.encuestador.IEncuestadorLlenado;
 
 @Controller
 public class ControlEncuestaController {
 	
 	@Autowired
 	private IControlEncuestaService ControlEncuestaService;
+	@Autowired
+	private IEncuestadorLlenado encuesta;
 	
 	@GetMapping("/revision")
 	private String EncuestasRevision(Model model)
@@ -31,5 +35,38 @@ public class ControlEncuestaController {
 		List<Encuesta> listaPila=ControlEncuestaService.mostrarPila();
 		model.addAttribute("revisar",listaPila);
 		return "revision/EncuestasRevisar";
+	}
+
+	
+	@GetMapping("/revisar/encuesta")
+	private String RevisarEncuesta(Model model)
+	{
+		List<Asignacion02> lista = encuesta.mostrar();
+		model.addAttribute("encuestas",lista);
+		return "revision/EncuestasRevisarCuestionario";
+	}
+
+	@GetMapping("/revision/encuesta")
+	private String RevisionEncuesta(Model model)
+	{
+		List<Asignacion02> lista = encuesta.mostrar();
+		model.addAttribute("encuestas",lista);
+		return "revision/EncuestasRevisionCuestionario";
+	}
+
+	@GetMapping("/revision/encuesta/preguntas")
+	private String RevisionEncuestaPreguntas(Model model)
+	{
+		List<Asignacion02> lista = encuesta.mostrar();
+		model.addAttribute("encuestas",lista);
+		return "revision/EncuestasRevisionPreguntas";
+	}
+
+	@GetMapping("/revisar/encuesta/preguntas")
+	private String RevisarEncuestaPreguntas(Model model)
+	{
+		List<Asignacion02> lista = encuesta.mostrar();
+		model.addAttribute("encuestas",lista);
+		return "revision/EncuestasRevisarPreguntas";
 	}
 }
